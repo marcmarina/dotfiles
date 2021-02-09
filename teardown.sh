84c73fa6b55c669083e23deb4fbd33fdf2402a56
Marc Marina Miravitlles <marc.marina.miravitlles@gmail.com>
2021-02-09 18:21:49 +0000

    Fixed teardown script

--- teardown.sh
+++ teardown.sh
@@ -1,3 +1,3 @@
 sudo apt purge -y zsh git neovim
 rm -rf ~/.oh-my-zsh ~/.zshrc ~/.vimrc ~/.zshrc.pre-oh-my-zsh ~/.config/nvim
-sudo usermod --shell /usr/bin/bash $USER
+sudo usermod --shell /bin/bash $USER
