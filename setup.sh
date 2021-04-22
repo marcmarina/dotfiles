96928bd759ddfdbdaae83ba63b4a0faa2e959f65
Marc Marina <mmarina@findmypast.com>
2021-04-22 17:18:16 +0100

    Fixed setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
+sudo apt update
 sudo apt install git zsh zsh-syntax-highlighting neovim ripgrep python3
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+mkdir -p ~/.config
 cp -r nvim ~/.config/nvim
 
 rm ~/.bashrc
