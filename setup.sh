4f595311a4da8f2ef6810c18209319268bfb778b
Marc Marina <mmarina@findmypast.com>
2021-11-20 22:34:26 +0000

    Restructured setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Removals
-rm ~/.zshrc
-
 # File copying
 mkdir -p ~/.config
-cp -r nvim ~/.config/nvim
-cp .gitconfig ~/.gitconfig
-
-# Symlinks
-ln -s $PWD/.vimrc /home/$USER/.vimrc
-ln -s $PWD/nvim/coc-settings.json /home/$USER/.config/nvim/coc-settings.json
-ln -s $PWD/.zshrc /home/$USER/.zshrc
+cat .gitconfig >> ~/.gitconfig
 
 # Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+rm ~/.zshrc
+
+# Symlinks
+ln -s $PWD/.zshrc /home/$USER/.zshrc
+ln -s $PWD/.vimrc /home/$USER/.vimrc
+ln -s $PWD/nvim /home/$USER/.config/nvim
 
 # Plugins/Packages
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
