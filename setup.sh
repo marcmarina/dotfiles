9086e9c7c3ef0514f330676f27b5977c89e1def6
Marc Marina <mmarina@findmypast.com>
2021-11-20 13:25:52 +0000

    Re-added asdf cloning step

--- setup.sh
+++ setup.sh
@@ -14,3 +14,4 @@ ln -s $PWD/.zshrc /home/$USER/.zshrc
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
