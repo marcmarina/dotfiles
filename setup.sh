fcd94a92b9a94a52ec915d335aa84e979c62dde7
Marc Marina <mmarina@findmypast.com>
2022-05-30 11:04:46 +0100

    Added tmux plugin manager

--- setup.sh
+++ setup.sh
@@ -20,3 +20,4 @@ ln -s $PWD/.tool-versions /home/$USER/.tool-versions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
--- teardown.sh
+++ teardown.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rm -rf ~/.oh-my-zsh ~/.zshrc ~/.vimrc ~/.zshrc.pre-oh-my-zsh ~/.config/nvim ~/.asdf ~/.fzf ~/.tool-versions ~/.tmux.conf
+rm -rf ~/.oh-my-zsh ~/.zshrc ~/.vimrc ~/.zshrc.pre-oh-my-zsh ~/.config/nvim ~/.asdf ~/.fzf ~/.tool-versions ~/.tmux.conf ~/.tmux
