8cfadc490722875381580e29e305a2a90c9491f3
Marc Marina <mmarina@findmypast.com>
2022-08-08 00:18:09 +0100

    Added keymaps

--- nvim/lua/marcmarina/keymaps.lua
+++ nvim/lua/marcmarina/keymaps.lua
@@ -8,12 +8,35 @@ keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Normal -- 
 -- Split navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
-
+-- Explore
 keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+
+-- Resizing
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Buffer navigation
+keymap("n", "<S-h>", ":bnext<CR>", opts)
+keymap("n", "<S-l>", ":bprevious<CR>", opts)
+
+-- Indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Save file
+keymap("n", "<C-s>", ":w<CR>", opts)
+
+-- Moving text
+-- keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+-- keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+
+-- Paste without yank
+keymap("v", "p", '"_dP', opts)
