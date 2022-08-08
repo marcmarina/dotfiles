e23e1c0bf34e0ce4c73080568b6a69cba113db1b
Marc Marina <mmarina@findmypast.com>
2022-08-08 09:56:59 +0100

    Treesitter WIP

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -69,6 +69,14 @@ return packer.startup(function(use)
 	use "neovim/nvim-lspconfig"
 	use "williamboman/nvim-lsp-installer"
 
+	-- Treesitter
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  }
+  use "p00f/nvim-ts-rainbow"
+  use "nvim-treesitter/playground"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
