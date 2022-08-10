5d1be3869031aaf23a76ad3b14949ec6cbeae13e
Marc Marina <mmarina@findmypast.com>
2022-08-10 21:31:20 +0100

    Added Lua lsp support

--- nvim/lua/config/lsp/lsp-installer.lua
+++ nvim/lua/config/lsp/lsp-installer.lua
@@ -17,8 +17,8 @@ lsp_installer.on_server_ready(function(server)
 	 end
 
 	 if server.name == "sumneko_lua" then
-	 	local sumneko_opts = require("config.lsp.settings.sumneko_lua")
-	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+		 local sumneko_opts = require("config.lsp.settings.sumneko_lua")
+		 opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
 	 end
 
 	-- This setup() function is exactly the same as lspconfig's setup function.
