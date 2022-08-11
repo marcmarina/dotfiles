61addb41ac33827863d9c31f12012f5b54a8053a
Marc Marina <mmarina@findmypast.com>
2022-08-11 09:16:05 +0100

    Refactored keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,52 +1,55 @@
-local opts = { noremap = true, silent = true }
-
 -- Keymap helper
-local keymap = vim.api.nvim_set_keymap
+local function keymap(mode, key, action)
+	vim.api.nvim_set_keymap(mode, key, action, {
+		noremap = true,
+		silent = true,
+	})
+end
 
 -- Remap leader
-keymap("", "<Space>", "<Nop>", opts)
+keymap("", "<Space>", "<Nop>")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Split navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-h>", "<C-w>h")
+keymap("n", "<C-j>", "<C-w>j")
+keymap("n", "<C-k>", "<C-w>k")
+keymap("n", "<C-l>", "<C-w>l")
 
 -- Explore
-keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+keymap("n", "<leader>e", ":Lex 30<cr>")
 
 -- Telescope
-keymap("n", "<leader>g", ":Telescope git_files<CR>", opts)
-keymap("n", "<leader>b", ":Telescope buffers<CR>", opts)
-keymap("n", "<leader>p", ":Telescope find_files<CR>", opts)
-keymap("n", "<leader>t", ":Telescope<CR>", opts)
+keymap("n", "<leader>g", ":Telescope git_files<CR>")
+keymap("n", "<leader>b", ":Telescope buffers<CR>")
+keymap("n", "<leader>p", ":Telescope find_files<CR>")
+keymap("n", "<leader>t", ":Telescope<CR>")
 
 -- Resizing
-keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-Down>", ":resize -2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize +2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Up>", ":resize +2<CR>")
+keymap("n", "<C-Down>", ":resize -2<CR>")
+keymap("n", "<C-Left>", ":vertical resize +2<CR>")
+keymap("n", "<C-Right>", ":vertical resize -2<CR>")
 
 -- Buffer navigation
-keymap("n", "<S-h>", ":bnext<CR>", opts)
-keymap("n", "<S-l>", ":bprevious<CR>", opts)
+keymap("n", "<S-h>", ":bnext<CR>")
+keymap("n", "<S-l>", ":bprevious<CR>")
 
 -- Indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+keymap("v", "<", "<gv")
+keymap("v", ">", ">gv")
 
 -- Save file
-keymap("n", "<C-s>", ":w<CR>", opts)
+keymap("n", "<C-s>", ":w<CR>")
 
 -- Paste without yank
-keymap("v", "p", '"_dP', opts)
-
+keymap("v", "p", '"_dP')
 
 -- NERDCommenter
-keymap("n", "++", "<Plug>NERDCommenterToggle", opts)
-keymap("v", "++", "<Plug>NERDCommenterToggle", opts)
+keymap("n", "++", "<Plug>NERDCommenterToggle")
+keymap("v", "++", "<Plug>NERDCommenterToggle")
 
 -- Alpha
-keymap("n", "<leader>m", ":Alpha <CR>", opts)
+keymap("n", "<leader>m", ":Alpha <CR>")
+
