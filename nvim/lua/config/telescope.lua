2e0fe0cffd6560929a13eaf3ccf33849fcc386d2
Marc Marina <mmarina@findmypast.com>
2022-08-11 08:08:01 +0100

    Changed telescope icons

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -10,7 +10,8 @@ local actions = require "telescope.actions"
 telescope.setup {
   defaults = {
 
-    selection_caret = " ",
+		prompt_prefix = " ",
+    selection_caret = " ",
 		path_display = { "smart" },
 
     mappings = {
