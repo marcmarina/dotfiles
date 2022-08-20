34d2fe0b007c40a242dc53af7351f49e3212aa2a
Marc Marina <mmarina@findmypast.com>
2022-08-20 15:36:59 +0200

    Added Github Copilot

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -93,6 +93,8 @@ return packer.startup(function(use)
   }
   use "AndrewRadev/tagalong.vim"
 
+  use "github/copilot.vim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
