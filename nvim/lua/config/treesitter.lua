d48f987d2af78eb3f9faf2700724ccd3650988bc
Marc Marina <marc.marina.miravitlles@gmail.com>
2022-09-09 22:20:13 +0100

    Tidy up of treesitter config

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -9,12 +9,15 @@ configs.setup {
     "bash",
   },
   sync_install = false,
-  ignore_install = { "" }, -- List of parsers to ignore installing
+  ignore_install = { "" },
   highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = {}, -- list of language that will be disabled
+    enable = true,
+    disable = {},
     additional_vim_regex_highlighting = true,
   },
-  indent = { enable = true, disable = { "yaml" } },
+  indent = {
+    enable = true,
+    disable = { "yaml" }
+  },
 }
 
