1968dcda8d8239c1fad6eb5e11c2a4bf58689eef
Marc Marina <marc.marina.miravitlles@gmail.com>
2023-01-22 15:32:52 +0000

    Added more scripts

---
+++ install_dependencies_linux.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install neovim git wget curl zsh tmux ripgrep fzf fd-find gcc g++
---
+++ setup_ppas.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo add-apt-repository ppa:git-core/ppa
+sudo add-apt-repository ppa:neovim-ppa/stable
