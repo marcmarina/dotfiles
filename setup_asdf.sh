ecce1e86dcc830ab171b2dd7864cfc4644fe6d6b
Marc Marina <marc.marina.miravitlles@gmail.com>
2023-01-28 13:31:33 +0000

    Fixed asdf setup script

--- setup_asdf.sh
+++ setup_asdf.sh
@@ -3,11 +3,14 @@ SCRIPTPATH=$(dirname $(readlink -f "$0"))
 
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
 
+ln -s $SCRIPTPATH/.tool-versions $HOME/.tool-versions
+
 asdf plugin add nodejs
 asdf plugin add yarn
 asdf plugin add lazygit
 asdf plugin add k9s https://github.com/looztra/asdf-k9s
 asdf plugin add lazydocker https://github.com/comdotlinux/asdf-lazydocker.git
 
-ln -s $SCRIPTPATH/.tool-versions $HOME/.tool-versions
-ln -s $SCRIPTPATH/lazygit $HOME/.config/lazygit
+ln -s $SCRIPTPATH/lazygit $HOME/.config
+
+asdf install
