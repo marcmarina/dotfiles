2aca6bc96404797353612cfd2a215ef48a1f5dbb
Marc Marina <marc.marina.miravitlles@gmail.com>
2023-01-28 13:10:17 +0000

    Added setup_asdf script

--- setup.sh
+++ setup.sh
@@ -21,5 +21,4 @@ ln -s $SCRIPTPATH/lazygit $HOME/.config/lazygit
 # Plugins/Packages
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
---
+++ setup_asdf.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
+
+asdf plugin add nodejs
+asdf plugin add yarn
+asdf plugin add lazygit
+asdf plugin add k9s https://github.com/looztra/asdf-k9s
+asdf plugin add lazydocker https://github.com/comdotlinux/asdf-lazydocker.git
+
