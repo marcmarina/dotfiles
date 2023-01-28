fff696f8687ad77a7e8e825744f0b187977120eb
Marc Marina <marc.marina.miravitlles@gmail.com>
2023-01-28 13:20:56 +0000

    Broken apart setup script

--- setup.sh
+++ setup.sh
@@ -3,22 +3,22 @@
 # Absolute path to script folder
 SCRIPTPATH=$(dirname $(readlink -f "$0"))
 
-# File copying
+# Create config folder
 mkdir -p ~/.config
-cp .gitconfig-base ~/.gitconfig
 
 # Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-rm ~/.zshrc
-
-# Symlinks
-ln -s $SCRIPTPATH/.zshrc $HOME/.zshrc
-ln -s $SCRIPTPATH/.tmux.conf $HOME/.tmux.conf
-ln -s $SCRIPTPATH/nvim $HOME/.config/nvim
-ln -s $SCRIPTPATH/.tool-versions $HOME/.tool-versions
-ln -s $SCRIPTPATH/lazygit $HOME/.config/lazygit
-
-# Plugins/Packages
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+rm ~/.zshrc
+ln -s $SCRIPTPATH/.zshrc $HOME/.zshrc
+
+# Git
+cp .gitconfig-base ~/.gitconfig
+
+# Tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -s $SCRIPTPATH/.tmux.conf $HOME/.tmux.conf
+
+# Neovim
+ln -s $SCRIPTPATH/nvim $HOME/.config/nvim
--- setup_asdf.sh
+++ setup_asdf.sh
@@ -1,3 +1,6 @@
+# Absolute path to script folder
+SCRIPTPATH=$(dirname $(readlink -f "$0"))
+
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
 
 asdf plugin add nodejs
@@ -6,3 +9,5 @@ asdf plugin add lazygit
 asdf plugin add k9s https://github.com/looztra/asdf-k9s
 asdf plugin add lazydocker https://github.com/comdotlinux/asdf-lazydocker.git
 
+ln -s $SCRIPTPATH/.tool-versions $HOME/.tool-versions
+ln -s $SCRIPTPATH/lazygit $HOME/.config/lazygit
