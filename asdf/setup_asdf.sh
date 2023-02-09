ccdca32ce99717794a96b6a4bdf32f9a7aca8fe6
Marc Marina <mmarina@findmypast.com>
2023-02-09 14:55:05 +0000

    Added kubectl version

--- asdf/setup_asdf.sh
+++ asdf/setup_asdf.sh
@@ -8,6 +8,7 @@ ln -s $SCRIPTPATH/.tool-versions $HOME/.tool-versions
 asdf plugin add nodejs
 asdf plugin add yarn
 asdf plugin add lazygit
+asdf plugin add kubectl
 asdf plugin add k9s https://github.com/looztra/asdf-k9s
 asdf plugin add lazydocker https://github.com/comdotlinux/asdf-lazydocker.git
 
