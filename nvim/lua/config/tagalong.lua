65542c7fb55fb12c5f92ca5869cb3e9f6ddff1c2
Marc Marina <marc.marina.miravitlles@gmail.com>
2023-09-29 16:15:57 +0200

    Removed Lua files

--- nvim/lua/config/tagalong.lua
+++
@@ -1,6 +0,0 @@
-vim.cmd [[
-
-let g:tagalong_filetypes = ['eco', 'eelixir', 'ejs', 'eruby', 'html', 'htmldjango', 'javascriptreact', 'jsx', 'php', 'typescriptreact', 'xml', 'javascript']
-
-]]
-
--- nvim/lua/helpers/keymaps.lua
+++
@@ -1,9 +0,0 @@
--- Keymap helper
-local function mapkey(mode, key, action)
-  vim.api.nvim_set_keymap(mode, key, action, {
-    noremap = true,
-    silent = true,
-  })
-end
-
-return mapkey
