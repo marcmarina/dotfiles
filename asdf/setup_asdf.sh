e203a16aadd9db18cb13020685d573265477f60b
Marc Marina <marc.marina.miravitlles@gmail.com>
2024-10-04 00:59:14 +0200

    Updated asdf script

--- asdf/setup_asdf.sh
+++ asdf/setup_asdf.sh
@@ -4,5 +4,6 @@ asdf plugin add lazygit
 asdf plugin add kubectl
 asdf plugin add k9s https://github.com/looztra/asdf-k9s
 asdf plugin add lazydocker https://github.com/comdotlinux/asdf-lazydocker.git
+asdf plugin add neovim
 
 asdf install
