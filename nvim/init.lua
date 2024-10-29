6d8fdb41560ba0a2f5d0e6dde8a1959e9b2e6979
Marc Marina <marc.marina.miravitlles@gmail.com>
2024-10-29 22:40:30 +0100

    Backup Tue Oct 29 22:40:30 CET 2024

--- nvim/init.lua
+++ nvim/init.lua
@@ -161,8 +161,6 @@ vim.opt.scrolloff = 10
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
-vim.keymap.set("n", "<C-s>", ":w<CR>")
-
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
