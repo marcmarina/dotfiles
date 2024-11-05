18e98ba6fc135ebae47e2104b4093b1a8dbf23f0
Marc Marina <mmarina@findmypast.com>
2024-11-05 11:18:10 +0100

    Backup Tue  5 Nov 2024 11:18:10 CET

--- nvim/init.lua
+++ nvim/init.lua
@@ -664,6 +664,9 @@ require("lazy").setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         "stylua", -- Used to format Lua code
+        "ts_ls",
+        "prettierd",
+        "eslint_d",
       })
       require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
