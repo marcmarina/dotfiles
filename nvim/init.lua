00f8acb247643e9c81b0ca49d0092e1884d0ab2d
Marc Marina <mmarina@findmypast.com>
2025-09-25 16:25:43 +0200

    Backup Thu 25 Sep 2025 16:25:43 CEST

--- nvim/init.lua
+++ nvim/init.lua
@@ -678,6 +678,7 @@ require("lazy").setup({
             -- by the server configuration above. Useful when disabling
             -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+            vim.lsp.enable(server_name)
             vim.lsp.config(server_name, server)
           end,
         },
